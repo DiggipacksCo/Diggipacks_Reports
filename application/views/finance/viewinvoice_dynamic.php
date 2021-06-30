987a7e6a89f7b76c95165a539216720e3895fe05
EC2 Default User <ec2-user@ip-172-31-93-160.ec2.internal>
2021-06-30 05:59:28 +0000

    update coed

--- application/views/finance/viewinvoice_dynamic.php
+++ application/views/finance/viewinvoice_dynamic.php
@@ -70,7 +70,7 @@
 						<!-- <img src="https://super.fastcoo-tech.com/assets/331.png.webp" height="70px;" />  -->
 					</td>
 
-					<td colspan="7"style="padding:2%;" ><b align="left">Name Of Company - <?= Getsite_configData_field('company_name'); ?>- اسم الشركة </b>
+					<td colspan="7"style="padding:2%;" ><b align="left">Name Of Company -   اسم الشركة -  <?= Getsite_configData_field('ligal_name'); ?> </b>
 						
 						<br/> <b>Vat Id No.:-&nbsp;<?= Getsite_configData_field('vat'); ?>- الرقم الضريبي </b>
 						<br/> <b>IBAN #:-&nbsp;<?=GetalldashboardClientField($invoiceData[0]['cust_id'], 'iban_number');?> </b>
